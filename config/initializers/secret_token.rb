dd605f4c5ea46be3db86bf84c404723d95772bc6
apoon- <anson.os.poon@gmail.com>
2013-11-04 19:45:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imigir::Application.config.secret_key_base = '9885a8b7f76be6e7746c5b7948d9ccddaca924cb4c1958c17e4c1dba55c38ad58f36dd4267ea7ad9f9830c6ec8ca7bec05c404af4aea326ee03ac873fe9f9dce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imigir::Application.config.session_store :cookie_store, key: '_imigir_session'
