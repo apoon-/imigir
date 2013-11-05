89a43efbdc65449c3d1dc1307e3ba47aee0d4130
apoon- <anson.os.poon@gmail.com>
2013-11-04 20:56:13 -0500

    adding create new picture using HTML form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -38,4 +38,17 @@ class PicturesController < ApplicationController
     ]
     @picture = @pictures[params[:id].to_i]
   end
+  def new 
+  end
+
+  def create
+    render :text => "Saving your picture. TITLE: #{params[:title]}, ARTIST: #{params[:artist]}, URL: #{params[:url]}"
+  end
+
+  # private
+
+  # def picture_params
+  #   params.require(:picture).permit(:title, :author, :url)
+  # end
+
 end
